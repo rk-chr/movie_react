775a80c944d3010b773500e7100be858bba81b63
kishore2486 <ramkishore157359@gmail.com>
2019-07-04 12:05:17 +0530

    changed the api url

--- axios.js
+++ axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3002',
+  baseURL: 'https://api.bingewatch.co.in',
 });
 
 instance.interceptors.request.use(
